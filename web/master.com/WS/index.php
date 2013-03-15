119edba1ff52f0773f30585e4da5946995e5cc2f
lessmore <funny@foxmail.com>
2013-03-15 16:30:00 +0800

    up

--- web/master.com/WS/index.php
+++ web/master.com/WS/index.php
@@ -7,7 +7,7 @@
 */
 require_once __DIR__.'/../../../sys/init.php';
 
-call('debug', array(2));
+cfg('debug_threshold',1);
 //call('reg', array('top',array('pro_start')));
 //call('reg', array('low','show_inc_file'));
 //call('reg', array('low',array('pro_end')));
